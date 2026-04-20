f310ca3cee13beeec3a6af96dea8f6b18696dcaa
Ksenya Kos <kskosyrevavv@gmail.com>
2026-04-20 19:02:52 +0300

    added code

--- src/Hw2Ex1.java
+++
@@ -1,2 +0,0 @@
-public class Hw2Ex1 {
-}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int ticketPrice = 8794;
+        int moneyForMile = 20;
+
+        int mile = ticketPrice / moneyForMile;
+        System.out.println(mile);
+    }
+}
